533e7eebea8855eeb17271c62062ee78ff038040
Martin Robles <robles.lprb@gmail.com>
2023-12-20 11:36:19 -0600

    Add all screens

--- lib/ui/screens/screens.dart
+++ lib/ui/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgets_app/ui/screens/animated/animated_screen.dart';
+export 'package:widgets_app/ui/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/ui/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/ui/screens/cards/cards_screen.dart';
 export 'package:widgets_app/ui/screens/home/home_screen.dart';
+export 'package:widgets_app/ui/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/ui/screens/progress/progress_screen.dart';
+export 'package:widgets_app/ui/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/ui/screens/ui_controls/ui_controls_screen.dart';
