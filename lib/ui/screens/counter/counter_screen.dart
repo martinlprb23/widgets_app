a9bc968e457207e18bc7fd798de17c15d4d7cb8d
Martin Robles <robles.lprb@gmail.com>
2023-12-26 12:27:28 -0600

    Add darkMode provider

--- lib/ui/screens/counter/counter_screen.dart
+++ lib/ui/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/ui/providers/counter_provider.dart';
+import 'package:widgets_app/ui/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = "counter_screen";
@@ -10,8 +11,20 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkTheme = ref.watch(darkThemeProvider);
     return Scaffold(
-      appBar: AppBar(title: const Text("Counter screen")),
+      appBar: AppBar(
+        title: const Text("Counter screen"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(darkThemeProvider.notifier).update((state) => !state);
+              },
+              icon: Icon(isDarkTheme
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined))
+        ],
+      ),
       body: Center(
         child: Text("Value: $clickCounter",
             style: Theme.of(context).textTheme.displayLarge),
